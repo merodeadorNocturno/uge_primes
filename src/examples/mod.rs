c72bf01784e5844dcbf96eec6754b3cee0a7c95e
tonatiuhmartinez <tonatiuh.martinez@globant.com>
2019-11-27 11:37:16 -0600

    Removed unused mods

--- src/examples/mod.rs
+++ src/examples/mod.rs
@@ -1,9 +1,9 @@
-pub mod two;
+// pub mod two;
 pub mod three;
-pub mod five;
-pub mod six;
-pub mod seven;
-pub mod eight;
-pub mod nine;
+// pub mod five;
+// pub mod six;
+// pub mod seven;
+// pub mod eight;
+// pub mod nine;
 pub mod ten;
-pub mod eleven;
+// pub mod eleven;
