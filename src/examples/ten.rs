8816da0b10dff3b3b352af32bd11156ba6eabebd
tonamartinez <merodeador.nocturno@gmail.com>
2019-11-26 22:09:23 -0600

    Removed some prints

--- src/examples/ten.rs
+++ src/examples/ten.rs
@@ -1,7 +1,7 @@
 use crate::examples::three::is_prime_32;
 
 pub fn counter (up_to: u64) -> Vec<u64> {
-  println!("Start");
+  // println!("Start");
   let mut my_vec: Vec<u64> = Vec::new();
 
   my_vec.push(2);
@@ -13,11 +13,11 @@ pub fn counter (up_to: u64) -> Vec<u64> {
       }
     }
   }
-  println!("Finish");
+  // println!("Finish");
   my_vec
 }
 
-pub fn sum_primes (my_vec: Vec<u64>) -> u64 {
+pub fn sum_primes (my_vec: &Vec<u64>) -> u64 {
   my_vec.iter().sum()
 }
 
